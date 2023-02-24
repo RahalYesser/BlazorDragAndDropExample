5012e091c4048bf7ebf0d870c679c1d6a7cb1b76
Rahal Yesser <rahalyesser9@gmail.com>
2023-02-24 13:29:48 +0100

    DragNDropExample

---
+++ BlazorDragAndDrop/Data/Models/jobModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorDragAndDrop.Data.Models
+{
+    public class JobModel
+    {
+        public int Id { get; set; }    
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public JobStatuses Status { get; set; }
+    }
+
+    public enum JobStatuses
+    {
+        Todo,
+        Started,
+        Progress,
+        Completed
+    }
+}
